70aeb6a133590291c330df11cee1a70f00650413
1826865187@qq.com <1826865187@qq.com>
2021-03-10 17:43:38 +0800

    初始化data

---
+++ lesson2/src/state.js
@@ -0,0 +1,37 @@
+import { observe } from "./observer/index";
+
+export function initState(vm) {
+  const opts = vm.$options;
+  if (opts.props) {
+    initProps(vm);
+  }
+
+  if (opts.methods) {
+    initMethods(vm);
+  }
+
+  if (opts.data) {
+    initData(vm);
+  }
+
+  if (opts.computed) {
+    initComputed(vm);
+  }
+
+  if (opts.watch) {
+    initWatch(vm);
+  }
+}
+
+function InitProps() {}
+function initMethods() {}
+
+// 数据的初始化操作
+function initData(vm) {
+    let data = vm.$options.data
+    data = typeof data === 'function' ? data.call(vm) : data;
+    observe(data)
+}
+
+function initComputed() {}
+function initWatch() {}
