53c989a2c121e7fcdbd78474e82275ac8cfe3aaa
wljzll <1826865187@qq.com>
2021-03-19 07:28:35 +0800

    数组劫持

--- lesson3/src/init.js
+++ lesson3/src/init.js
@@ -1,8 +1,7 @@
-import Vue from ".";
 import { initState } from "./state";
 
 // 在这里进行各种初始化操作
-export function initMixin (Vue) {
+export function initMixin(Vue) {
     Vue.prototype._init = function(options) {
         const vm = this;
         vm.$options = options
