44d3cbc19fb26a0ff6e15e460c7e68653d797bfb
wljzll <1826865187@qq.com>
2021-04-01 23:27:39 +0800

    ast

--- lesson3+/src/init.js
+++ lesson3+/src/init.js
@@ -1,24 +1,35 @@
+import { compileToFunctions } from "./compiler/index";
 import { initState } from "./state";
 
 // 在这里进行各种初始化操作
 export function initMixin(Vue) {
-  Vue.prototype._init = function (options) {
-    const vm = this;
-    vm.$options = options;
+    Vue.prototype._init = function(options) {
+        const vm = this;
+        vm.$options = options;
 
-    // 初始化状态
-    initState(vm);
+        // 初始化状态
+        initState(vm);
 
-    if (vm.$options.el) {
-      vm.$mount(vm.options.el);
-    }
-  };
+        if (vm.$options.el) {
+            vm.$mount(vm.$options.el);
+        }
+    };
 
-  Vue.prototype.$mount = function (el) {
-    //  挂载操作
-    const vm = this;
-    const options = vm.$options;
-    el = document.querySelector(el);
+    Vue.prototype.$mount = function(el) {
+        //  挂载操作
+        const vm = this;
+        const options = vm.$options;
+        el = document.querySelector(el);
 
-  };
+        if (!options.render) {
+            // 没有render 将template转化成render方法
+            let template = options.template;
+            if (!template && el) {
+                template = el.outerHTML;
+            }
+            // 编译原理 将模板编译成render函数
+            const render = compileToFunctions(template);
+            options.render = render;
+        }
+    };
 }
